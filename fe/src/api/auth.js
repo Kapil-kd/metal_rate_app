a60052ac1d92a18783331594c840167584307ae4
Kapil M <100362362+Kapil-kd@users.noreply.github.com>
2025-08-22 19:01:31 +0530

    Update auth.js

--- fe/src/api/auth.js
+++ fe/src/api/auth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: `${process.env.REACT_APP_SERVER}/api/auth` });
+// const API = axios.create({ baseURL: `${process.env.REACT_APP_SERVER}/api/auth` });
+const API = axios.create({ baseURL: `https://metal-rate-app.onrender.com/api/auth` });
 
 export const loginUser = async (username, password) => {
   const res = await API.post('/login', { username, password });
   return res.data.token;
 };
+
