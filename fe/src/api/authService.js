767c518a8629b949e3292731f354d86fdd6231e0
Kapil M <100362362+Kapil-kd@users.noreply.github.com>
2025-08-22 19:01:52 +0530

    Update authService.js

--- fe/src/api/authService.js
+++ fe/src/api/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: `${process.env.REACT_APP_SERVER}/api` });
+const API = axios.create({ baseURL: `https://metal-rate-app.onrender.com/api` });
 
 export const signup = (userData) => API.post('/auth/signup', userData);
 export const login = (credentials) => API.post('/auth/login', credentials);
@@ -9,4 +9,5 @@ export const verifylogin = (token) =>
   API.get(`/auth/loginverify`, { headers: { Authorization: `Bearer ${token}` } });
 
 export const Logoutfn = (token) =>
+
   API.get(`/auth/logout`, { headers: { Authorization: `Bearer ${token}` } });
